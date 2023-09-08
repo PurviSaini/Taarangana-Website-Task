9b12477197b4aa9a7ed73f46d122db654f9f9ba2
Purvi Saini <purvitashu450@gmail.com>
2023-09-08 19:28:35 +0530

    pre loader added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-
-    console.log("ehlskf")
+      $("#loading").css("display","none");
+      
     $(".nav-link").on("click",function(e){
         $(".nav-link").removeClass("active");
         $(this).addClass("active");
