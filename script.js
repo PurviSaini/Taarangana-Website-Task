b4986e77be76b7b46a7c4ab73995e93095fc3993
Purvi Saini <purvitashu450@gmail.com>
2023-09-08 19:21:06 +0530

    jquery added | active nav link feature added

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    console.log("ehlskf")
+    $(".nav-link").on("click",function(e){
+        $(".nav-link").removeClass("active");
+        $(this).addClass("active");
+      })
+  
+  });
+
